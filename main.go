10a918c5ae2ff2f9114ceca462c598530becde0f
hadi ali <haadii.ali.2007@gmail.com>
2026-09-04 12:41:46 +0300

    Added memory and cpu limit (100mb,50,000μs)

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"syscall"
 )
 
 const rootfs = "/home/hadiali/Projects/myDocker/rootfs"
-
+func generateID() string {
+    b := make([]byte, 4)
+    rand.Read(b)
+    return hex.EncodeToString(b)
+}
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "child" {
 		if err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""); err != nil {
 			panic(err)
+		}
+if err := syscall.Mkdir(rootfs+"/proc", 0755); err != nil && !os.IsExist(err) {
+    	panic(err)
 		}
 		if err := syscall.Mkdir(rootfs+"/.oldroot", 0755); err != nil && !os.IsExist(err) {
 			panic(err)
@@ -25,6 +36,7 @@ func main() {
 		if err := syscall.Chdir("/"); err != nil {
 			panic(err)
 		}
+		
 		if err := syscall.Mount("proc", "/proc", "proc", 0, ""); err != nil {
 			panic(err)
 		}
@@ -40,11 +52,32 @@ func main() {
 			Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
 			Setsid:     true,
 		}
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			panic(err)
-		}
-	}
+		containerID:= generateID()
+		if err := os.MkdirAll("/sys/fs/cgroup/mydocker/"+containerID, 0755); err != nil {
+    panic(err)
+}
+
+if err := os.WriteFile("/sys/fs/cgroup/mydocker/"+containerID+"/memory.max", []byte("100000000"), 0644); err != nil {
+    panic(err)
+}
+if err := os.WriteFile("/sys/fs/cgroup/mydocker/"+containerID+"/cpu.max", []byte("50000 100000"), 0644); err != nil {
+    panic(err)
+}
+cmd.Stdin = os.Stdin
+cmd.Stdout = os.Stdout
+cmd.Stderr = os.Stderr
+if err := cmd.Start(); err != nil {
+    panic(err)
+}
+
+pid := cmd.Process.Pid
+if err := os.WriteFile("/sys/fs/cgroup/mydocker/"+containerID+"/cgroup.procs", []byte(strconv.Itoa(pid)), 0644); err != nil {
+    panic(err)
+}
+
+fmt.Println("Container ID:", containerID)
+cmd.Wait()
+os.RemoveAll("/sys/fs/cgroup/mydocker/" + containerID)
+}
+	
 }
